9f9f1e7ea5ee9c4b37c80efeecb69c37c55a3611
yasmeen2006 <yasmeenfirdous457@gmail.com>
2026-06-15 21:23:51 +0530

    Update appconfig.js

--- appconfig.js
+++ appconfig.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    GROQ_API_KEY: "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",
+    GROQ_API_KEY: "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",
     WEATHER_API_KEY: "40b87d7e76ff2fc8da7c4c0cfaa8cba7"
 };
